69e2f9de958f190908d7b4014088aa5330d7be33
Elena Kulgavaya <elena.kulgavaya@gmail.com>
2026-05-09 17:51:18 +0200

    Deprecate fild

--- fild_db/__init__.py
+++ fild_db/__init__.py
@@ -1,3 +1,15 @@
+import warnings
+
+# pylint: disable=wrong-import-position
+warnings.warn(
+    "fild-db is deprecated and will no longer be maintained. "
+    "Migrate to surety-db: pip install surety-db. "
+    "Replace 'from fild_db import ...' with 'from surety.db import ...'. "
+    "See https://github.com/elenakulgavaya/surety-db for details.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 from .types import mysql
 from .client import MysqlDBClient, PostgresqlDBClient, SqliteDBClient
 from .database import Database
